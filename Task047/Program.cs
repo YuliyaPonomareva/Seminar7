c0efd77a028195f4cf72a4fa2779b018d8b25470
yuliya <liana-yuliana@list.ru>
2023-06-15 22:06:31 +0300

    Added decision

--- Task047/Program.cs
+++ Task047/Program.cs
@@ -5,3 +5,41 @@ m = 3, n = 4.
 0,5 7 -2 -0,2
 1 -3,3 8 -9,9
 8 7,8 -7,1 9   */
+
+Console.Clear();
+int rows = Prompt("Введите количество строк массива: ");
+int columns = Prompt("Введите количество столбцов массива: ");
+double [,] array = GetArray(rows, columns, -1000, 1001);
+PrintArray(array);
+
+int Prompt (string messange)
+{
+    Console.Write(messange);
+    int number = int.Parse(Console.ReadLine()!);
+    return number;
+}
+
+double [,] GetArray (int m, int n, int minValue, int maxValue)
+{
+    double [,] result = new double [m,n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i,j] = Math.Round(new Random().Next(minValue,maxValue)/100.00, 1);
+        }
+    }
+    return result;
+}
+ 
+ void PrintArray (double [,] arr)
+ {
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+ }
--- Task052/Program.cs
+++ Task052/Program.cs
@@ -7,3 +7,57 @@
 8 4 2 4
 Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.  */
 
+Console.Clear();
+int rows = Prompt("Введите количество строк массива: ");
+int columns = Prompt("Введите количество столбцов массива: ");
+int [,] array = GetArray(rows, columns, 0, 10);
+PrintArray(array);
+AverageDate (array);
+
+int Prompt (string messange)
+{
+    Console.Write(messange);
+    int number = int.Parse(Console.ReadLine()!);
+    return number;
+}
+
+int [,] GetArray (int m, int n, int minValue, int maxValue)
+{
+    int [,] result = new int [m,n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i,j] = new Random().Next(minValue,maxValue);
+        }
+    }
+    return result;
+}
+ 
+ void PrintArray (int [,] arr)
+ {
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+ }
+
+ void AverageDate (int [,] inArr)
+ {
+    double end = 0;
+    for (int j = 0; j < inArr.GetLength(1); j++)
+    {
+        double sum = 0;
+        for (int i = 0; i < inArr.GetLength(0); i++)
+        {
+            sum += inArr[i,j];
+        }
+        end = Math.Round(sum / rows, 2);
+        Console.Write($"{end} ");
+    }
+    Console.Write("- Среднее арифметическое элементов каждого столбца.");
+}
