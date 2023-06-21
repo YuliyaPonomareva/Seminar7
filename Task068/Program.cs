d13b4f1ae0a6632fe0a349f7ded8b6052daa0c81
YuliyaPonomareva <131998306+YuliyaPonomareva@users.noreply.github.com>
2023-06-22 00:18:48 +0300

    removed comments

--- Task068/Program.cs
+++ Task068/Program.cs
@@ -45,19 +45,3 @@ int AсkermanFunction (int firstDigit, int secondDigit)
     return AсkermanFunction(firstDigit - 1, AсkermanFunction(firstDigit, secondDigit - 1));
   }
 }
-
-
-
-// int Ack(int m, int n)
-// {
-//   if (m == 0) return n + 1;
-//   else if (n == 0) return Ack(m - 1, 1);
-//   else return Ack(m - 1, Ack(m, n - 1));
-// }
-
-// int InputNumbers(string input) 
-// {
-//   Console.Write(input);
-//   int output = Convert.ToInt32(Console.ReadLine());
-//   return output;
-// }
